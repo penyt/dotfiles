9a3dab9477d07284b78094960aafef41281ae950
penyt <160297029+penyt@users.noreply.github.com>
2026-06-06 16:42:19 +0800

    0605

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,18 +6,16 @@ local config = wezterm.config_builder()
 
 -- ⬇ This is where you actually apply your config choices.
 
-
-
-
-config.initial_cols = 60
-config.initial_rows = 28
-
--- or, changing the font size and color scheme.
-config.font_size = 16
-config.color_scheme = 'iTerm2 Dark Background'
-config.font = wezterm.font 'CaskaydiaMono NF'
-
+-- Windows behavior
+-- config.initial_cols = 60
+-- config.initial_rows = 28
 config.window_close_confirmation = 'NeverPrompt'
+
+-- Font size & Color scheme
+config.font_size = 16
+config.font = wezterm.font 'CaskaydiaMono NF'
+config.color_scheme = 'Apple System Colors'
+
 
 -- ⬆ End of your config.
 
